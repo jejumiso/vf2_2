09be1aa31c9922ef1da4234a2fdc85aeda6b63e2
jejumiso <jejumiso@gmail.com>
2021-04-02 13:28:20 +0900

    firebase init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">3</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>타이틀</v-toolbar-title>
+      <v-spacer />
+      <v-btn icon to="/about">s </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  name: "App",
+};
+</script>
